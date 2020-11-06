00fb8e75149090c1689926b855a70f0c48b0164b
Akash <akash.grep@gmail.com>
2020-11-06 17:14:57 +0530

    Added routes product categories

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
-
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
 // My Routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+
 //DB Connection
 mongoose
   .connect(process.env.DATABASE, {
@@ -28,6 +30,7 @@ app.use(cors());
 //My Routes
 app.use("/api", authRoutes);
 app.use("/api",userRoutes);
+app.use("/api", categoryRoutes);
 //PORT
 const port = process.env.PORT || 6000;
 
