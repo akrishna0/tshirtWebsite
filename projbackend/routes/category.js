cb05c99612acef981b7d1e43533384844ed2931a
Akash <akash.grep@gmail.com>
2020-11-06 17:15:37 +0530

    Written starting routes for parameters and imported required controllers from controllers -> auth, user and category

---
+++ projbackend/routes/category.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const {getCategoryById} = require('../controllers/category')
+const {isSignedIn,isAdmin, isAuthenticated} = require('../controllers/auth')
+const {getUserById} = require('../controllers/user')
+
+// PARAMS
+router.param("userId", getUserById);
+router.param("categoryId", getCategoryById);
+
+//ACTUAL ROUTES
+router.post("/category/create/:userId",isSignedIn,isAuthenticated,isAdmin, createCategory);
+
+module.exports = router;
