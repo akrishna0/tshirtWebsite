5b84f60a70f8d3441fd4f561545009aac6adbb1e
Akash <akash.grep@gmail.com>
2020-11-20 02:04:45 +0530

    Completed product routes and controllers

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
-const {getProductById, createProduct} = require("../controllers/product")
+const {getProductById, createProduct, getProduct, photo, removeProduct, updateProduct, getAllProducts, getAllUniqueCategories} = require("../controllers/product")
 
 // PARAMETERS
 router.param("userId", getUserById);
@@ -11,4 +11,14 @@ router.param("productId", getProductById);
 
 // ACTUAL ROUTES
 router.post("/product/create/:userId",isSignedIn, isAuthenticated, isAdmin, createProduct);
+// read
+router.get("/product/:productId",getProduct);
+router.get("product/photo/:productId", photo);
+
+router.delete("/product/:productId/:userId",isSignedIn, isAuthenticated, isAdmin, removeProduct);
+router.put("/product/:productId/:userId",isSignedIn, isAuthenticated, isAdmin, updateProduct);
+
+router.get("/products",getAllProducts);
+
+router.get("products/categories",getAllUniqueCategories);
 module.exports = router;
