75895d992fc59e8ad808ee0439feb558d4aea354
Akash <akash.grep@gmail.com>
2020-12-05 23:39:24 +0530

    routes related to orders are written

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -4,10 +4,22 @@ const router = express.Router();
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const {getUserById, pushOrderInPurchaseList} = require("../controllers/user")
 const {updateInventory} = require('../controllers/product')
-const {getOrderById} = require("../controllers/order")
+const {getOrderById, createOrder, getAllOrders,getOrderStatus,updateStatus} = require("../controllers/order")
 
 // params
 router.param("userId", getUserById);
 router.param("orderId", getOrderById);
 // Routes
-module.exports = router;
+router.post(
+    "/orer/create/:userId",
+    isSignedIn,
+    isAuthenticated, 
+    pushOrderInPurchaseList, 
+    updateInventory, 
+    createOrder
+    );
+
+router.get("/order/all/:userId",isSignedIn, isAuthenticated,isAdmin, getAllOrders);
+router.get("/order/status/:userId",isSignedIn, isAuthenticated,getOrderStatus);
+router.put("order/:orderId/status/:userId", isSignedIn, isAuthenticated, isAdmin, updateStatus);
+module.exports = router;    
