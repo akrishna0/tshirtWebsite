01f1fbfe7ccff9e584376d9828c49ab902388b96
Akash <akash.grep@gmail.com>
2020-12-05 23:38:57 +0530

    controllers for orders in the product

--- projbackend/controllers/order.js
+++ projbackend/controllers/order.js
@@ -14,5 +14,44 @@ exports.getOrderById = (req, res, next, id) =>{
 }
 
 exports.createOrder = (req, res) => {
-    req.body.order.user = req.profile
+    req.body.order.user = req.profile;
+    const order = new Order(req.body.order);
+    order.save((err, order) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Failed to Save Order in  DB",
+            })
+        }
+        res.json(order)
+    })
+}
+
+exports.getAllOrders = (req, res)=>{
+    Order.find()
+    .populate("user", "_id name")
+    .exec((err, orders)=>{
+        if(err) {
+            return res.status(400).json({
+                error: "no Order found in Database",
+            });
+        }
+        res.json(orders);
+    })
+}
+
+exports.getOrderStatus =(req, res)=>{
+    res.json(Order.schema.path("status").enumValues);
+}
+exports.updateStatus = (req, res)=>{
+    Order.update(
+        {_id: req.body.orderId},
+        {$set: {status: req.body.status}},
+        (err, order)=>{
+            if(err) {
+                return res.status(400).json({
+                    error:"Can't update Order"});
+            }
+            res.json(order);
+        }
+    )
 }
