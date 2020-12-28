d7bf3cf21c55f5472d4b846510067d20b5f75117
Akash Krishna <akash.grep@gmail.com>
2020-12-29 00:02:33 +0530

    Added and written Base Component in the core dir
    - And used it in Home Page

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Menu from './Menu';
+
+const Base=({
+    title = "My Title",
+    description = "My Description",
+    className = "bg-dark text-white",
+    children 
+})=>(
+    <div>
+        <Menu />
+        <div className="container-fluid">
+            <div className="jumbotron bg-dark text-white text-center">
+                <h2 className="display-4">{title}</h2>
+                <p className="lead">{description}</p>
+            </div>
+            <div className={className}>{children}</div>
+        </div>
+        <footer className="footer bg-dark mt-auto py-3">
+            <div className="container-fluid bg-success text-white text-center  py-3">
+                <h4>Feel Free to ask any questions</h4>
+                <button className="btn btn-warning btn-lg">Contact Us</button>
+            </div>
+            <div className="container">
+                <span className="text-muted"> MERN PROJECT</span>
+            </div>
+        </footer>
+    </div>
+);
+
+export default Base;
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../styles.css'
 import {API} from '../backend'
+import Base from './Base';
+
 function Home() {
     console.log("API IS THIS", API);
     return (
-        <div>
-            <h1 className="text-white">Home</h1>
-        </div>
+        <Base title="Home Page" description="This is the First Page">
+            <h1 className="text-white">Home is this</h1>
+        </Base>
     )
 }
 
