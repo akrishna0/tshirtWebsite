b350c97b91516eed232a8ca87e74b835889a52f4
Akash Krishna <akash.grep@gmail.com>
2020-12-29 00:39:39 +0530

    Added Routes for Signup and SignIn

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './core/Home'
+import Signup from './user/Signup';
+import SignIn from './user/Signin'
 const Routes = () =>{
     return (
         <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/signin" exact component={SignIn} />
             
         </Switch>
         </BrowserRouter>
