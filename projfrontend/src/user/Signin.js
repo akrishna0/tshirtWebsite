d7d114fd91dafb477e4c0acff9291defb381a8ad
Akash Krishna <akash.grep@gmail.com>
2021-01-21 01:57:42 +0530

    Signin Functionality complete

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,33 +1,125 @@
-import React from 'react'
-import Base from '../core/Base'
-import {Link} from 'react-router-dom'
-const Signin = ()=> {
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link, Redirect } from "react-router-dom";
+import { signin, authenticate, isAuthenticated } from "../auth/helper/index";
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAuthenticated;
 
-    const signinForm = () =>{
-        return(
-            <div className="row">
-                <div className="col-md-4 offset-sm-4 text-left">
-                    <form>
-                        <div className="form-group">
-                            <label className="text-light">Email</label>
-                            <input type="Email" className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label className="text-light">Password</label>
-                            <input type="password" className="form-control"/>
-                        </div>
-                        <button type="submit" className="btn btn-success btn-block">Sign In</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then((data) => {
+      if (data.errors) {
+        setValues({ ...values, error: data.errors, loading: false });
+      } else {
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            didRedirect: true,
+          });
+        });
+      }
+    })
+    .catch(console.log("SIGNIN FAILED"))
+  };
+
+  const performRedirect =()=>{
+      if(didRedirect){
+          if(user && user.role === 1){
+              return(<p>redirect to admin dashboard</p>)
+          }else{
+              return(<p>redirect to user dashboard</p>)
+          }
+      }
+      if(isAuthenticated()){
+          return <Redirect to="/" />
+      }
+  }
+
+  const loadingMessage = () => {
     return (
-        <Base title="Sign In Page" description="This is a signin page">
-            {signinForm()}
-        </Base>
-    )
-}
+      <div className="row">
+        <div className="col-md-4 offset-sm-4 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: loading ? "" : "none" }}
+          >
+              <h1>Loading......</h1>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-4 offset-sm-4 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  const signinForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-4 offset-sm-4 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                onChange={handleChange("email")}
+                value={email}
+                type="Email"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                onChange={handleChange("password")}
+                value={password}
+                type="password"
+                className="form-control"
+              />
+            </div>
+            <button
+              onClick={onSubmit}
+              type="submit"
+              className="btn btn-success btn-block"
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
 
-export default Signin
+  return (
+    <Base title="Sign In Page" description="This is a signin page">
+      {loadingMessage()}
+      {errorMessage()}
+      {signinForm()}
+      {performRedirect()}
+    </Base>
+  );
+};
+
+export default Signin;
