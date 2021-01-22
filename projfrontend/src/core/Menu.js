c46703af1700d7d7d8d34881a628c2c72e56b6db
Akash Krishna <akash.grep@gmail.com>
2021-01-22 16:23:41 +0530

    Restrict SIgnup and Signin to render after login

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,56 +1,88 @@
-import React from 'react'
-import {Link, withRouter} from 'react-router-dom'
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth/helper/index";
 
-const currentTab = (history,path)=>{
-    if(history.location.pathname === path){
-        return {color: '#32db2c'};
-    }
-        return {color: '#FFFFFF'};
-    
-}
+const currentTab = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#32db2c" };
+  }
+  return { color: "#FFFFFF" };
+};
 
-const Menu=({history})=> (
-    <div>
-        <ul className="nav nav-tabs bg-dark">
-            <li className="nav-item">
-                <Link style={currentTab(history, "/")} to="/" className="nav-link">
-                    Home
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, "/cart")} to="/cart" className="nav-link">
-                 Cart
-                </Link>
-            </li>
+const Menu = ({ history }) => (
+  <div>
+    <ul className="nav nav-tabs bg-dark">
+      <li className="nav-item">
+        <Link style={currentTab(history, "/")} to="/" className="nav-link">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          style={currentTab(history, "/cart")}
+          to="/cart"
+          className="nav-link"
+        >
+          Cart
+        </Link>
+      </li>
 
-            <li className="nav-item">
-                <Link style={currentTab(history, "/user/dashboard")} to="/user/dashboard" className="nav-link">
-                    Dashboard
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, "/admin/dashboard")} to="/admin/dashboard" className="nav-link">
-                    A. Dashboard
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, "/signup")} to="/signup" className="nav-link">
-                    Signup
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, "/signin")} to="/signin" className="nav-link">
-                    SignIn
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, "/signout")} to="/signout" className="nav-link">
-                    SignOut
-                </Link>
-            </li>
-        </ul>        
-    </div>
-   
+      <li className="nav-item">
+        <Link
+          style={currentTab(history, "/user/dashboard")}
+          to="/user/dashboard"
+          className="nav-link"
+        >
+          Dashboard
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          style={currentTab(history, "/admin/dashboard")}
+          to="/admin/dashboard"
+          className="nav-link"
+        >
+          A. Dashboard
+        </Link>
+      </li>
+      {!isAuthenticated() && (
+        <Fragment>
+          <li className="nav-item">
+            <Link
+              style={currentTab(history, "/signup")}
+              to="/signup"
+              className="nav-link"
+            >
+              Signup
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              style={currentTab(history, "/signin")}
+              to="/signin"
+              className="nav-link"
+            >
+              SignIn
+            </Link>
+          </li>
+        </Fragment>
+      )}
+      {isAuthenticated() && (
+        <li className="nav-item">
+          <span
+            className="nav-link text-warning"
+            onClick={() => {
+              signout(() => {
+                history.push("/");
+              });
+            }}
+          >
+            SignOut
+          </span>
+        </li>
+      )}
+    </ul>
+  </div>
 );
 
 export default withRouter(Menu);
