83c33a94694376797c5241e5c58eeb532367bc2c
Akash Krishna <akash.grep@gmail.com>
2021-02-28 03:23:56 +0530

    Write AdminRoutes PrivateRoutes Profile and Dashboards

---
+++ projfrontend/src/user/AdminDashBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Base from "../core/Base";
+const AdminDashBoard =()=> {
+    return (
+        <Base title="AdminDashBoard Page">
+            <h1>This is the AdminDashBoard Page</h1>
+        </Base>
+    )
+}
+
+export default AdminDashBoard;
