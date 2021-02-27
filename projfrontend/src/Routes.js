ab95d0a50f8ce771e9ce7441adb8561c15f6d0c3
Akash Krishna <akash.grep@gmail.com>
2021-02-28 03:47:23 +0530

    Add All Private and Admin Routes in Routes.js

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Home from './core/Home'
-import Signup from './user/Signup';
-import SignIn from './user/Signin'
-const Routes = () =>{
-    return (
-        <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/signup" exact component={Signup} />
-            <Route path="/signin" exact component={SignIn} />
-            
-        </Switch>
-        </BrowserRouter>
-    )
-}
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./core/Home";
+import Signup from "./user/Signup";
+import SignIn from "./user/Signin";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={SignIn} />
+        <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
