8c842f98af51d78ee6cc07240d366fffe3cc2098
Akash Krishna <akash.grep@gmail.com>
2021-03-08 00:42:19 +0530

    Category Section to add category is created

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { isAuthenticated } from "../auth/helper";
 import Base from "../core/Base";
+import { createCategory } from "./helper/adminapicall";
 
 const AddCategory = () => {
   const [name, setName] = useState();
   const [error, setError] = useState(false);
-  const [sucess, setSucess] = useState(false);
+  const [success, setSuccess] = useState(false);
   const { user, token } = isAuthenticated();
 
   const goBack = () => (
@@ -16,6 +17,39 @@ const AddCategory = () => {
       </Link>
     </div>
   );
+
+  const handleChange = (event) => {
+    setError("");
+    setName(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setError("");
+    setSuccess(false);
+
+    // backend request
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.errors) {
+        console.log("THIS IS THE ERR", data.errors);
+        setError(data.error);
+      } else {
+        setError("");
+        setSuccess(true);
+        setName("");
+      }
+    });
+  };
+  const successMessage = () => {
+    if (success) {
+      return <h4 className="text-success"> Category Created Successfully</h4>;
+    }
+  };
+  const errorMessage = () => {
+    if (error) {
+      return <h4 className="text-danger"> There was an error</h4>;
+    }
+  };
   const myCategoryForm = () => (
     <form>
       <div className="form-group">
@@ -23,11 +57,15 @@ const AddCategory = () => {
         <input
           type="text"
           className="form-control my-3"
+          onChange={handleChange}
+          value={name}
           autoFocus
           required
           placeholder="For Example Summer"
         />
-        <button className="btn btn-outline-info">Create Category</button>
+        <button onClick={onSubmit} className="btn btn-outline-info">
+          Create Category
+        </button>
       </div>
     </form>
   );
@@ -40,6 +78,8 @@ const AddCategory = () => {
     >
       <div className="row bg-white rounded">
         <div className="col-md-8 offset-md-2">
+          {successMessage()}
+          {errorMessage()}
           {myCategoryForm()}
           {goBack()}
         </div>
