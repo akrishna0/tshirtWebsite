19dffd213f004d16e37b18e2b272344ff56a21a5
Akash Krishna <akash.grep@gmail.com>
2021-03-08 00:41:50 +0530

    Remove Extra data of role of no use

--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -4,7 +4,7 @@ import { isAuthenticated } from "../auth/helper/index";
 import { Link } from "react-router-dom";
 const AdminDashBoard = () => {
   const {
-    user: { name, email, role },
+    user: { name, email },
   } = isAuthenticated();
 
   const adminLeftSide = () => {
