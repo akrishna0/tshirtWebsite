48b3d613bd72380d1e73659a3ad8ad6e2082b84f
Akash Krishna <akash.grep@gmail.com>
2021-03-07 23:43:16 +0530

    Admin Dashboard Layout

--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -1,11 +1,84 @@
-import React from 'react';
+import React from "react";
 import Base from "../core/Base";
-const AdminDashBoard =()=> {
+import { isAuthenticated } from "../auth/helper/index";
+import { Link } from "react-router-dom";
+const AdminDashBoard = () => {
+  const {
+    user: { name, email, role },
+  } = isAuthenticated();
+
+  const adminLeftSide = () => {
     return (
-        <Base title="AdminDashBoard Page">
-            <h1>This is the AdminDashBoard Page</h1>
-        </Base>
-    )
-}
+      <div className="card">
+        <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link
+              to="/admin/create/category/"
+              className="nav-link text-success"
+            >
+              Create Categories
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link
+              to="/admin/create/category/"
+              className="nav-link text-success"
+            >
+              Create Products
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link
+              to="/admin/create/category/"
+              className="nav-link text-success"
+            >
+              Manage Orders
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link
+              to="/admin/create/category/"
+              className="nav-link text-success"
+            >
+              Manage Products
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  const adminRightSide = () => {
+    return (
+      <div className="card mb-4">
+        <h4 className="card-header">Admin Info</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <span className="badge badge-success mr-2"> Name: </span>
+            {name}
+          </li>
+          <li className="list-group-item">
+            <span className="badge badge-success mr-2"> Email: </span> {email}
+          </li>
+          <li className="list-group-item">
+            <span className="badge badge-danger mr-2"> Admin Area </span>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  return (
+    <Base
+      title="Welcome to Admin zone"
+      description="Manage all of the products here"
+      className="container bg-success p-4"
+    >
+      <div className="row">
+        <div className="col-3">{adminLeftSide()}</div>
+        <div className="col-9">{adminRightSide()}</div>
+      </div>
+    </Base>
+  );
+};
 
 export default AdminDashBoard;
