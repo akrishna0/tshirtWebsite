cea28fba2d6231ff7bfa3f00f8f2b5cb0a83e33b
Akash <akash.grep@gmail.com>
2021-04-18 15:12:48 +0530

    Route and Controllers for payment

---
+++ projbackend/controllers/payment.js
@@ -0,0 +1,33 @@
+const braintree = require("braintree");
+
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "useYourMerchantId",
+  publicKey: "useYourPublicKey",
+  privateKey: "useYourPrivateKey",
+});
+
+exports.getToken = (req, res) => {
+  gateway.clientToken.generate({}, (err, response) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(response);
+    }
+  });
+};
+exports.processPayment = (req, res) => {
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+
+  const amountFromTheClient = req.body.amount;
+  gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+      options: {
+        submitForSettlement: true,
+      },
+    },
+    (err, result) => {}
+  );
+};
